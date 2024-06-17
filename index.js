4c90885f4716c7635b61e141b01ef846523ae297
John Irungu <johnirungu977@gmail.com>
2024-06-17 12:53:00 +0300

    Production Build Complete

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
+
+app.use(express.static("dist"));
+
 app.use(express.json());
+
+// Configure morgan to log HTTP requests in 'tiny' format
+app.use(morgan("tiny"));
+
+// Custom token to log the body of POST requests
+morgan.token("body", (req) => JSON.stringify(req.body));
+
+// Custom format that includes the body of POST requests
+const customFormat =
+  ":method :url :status :res[content-length] - :response-time ms :body";
+
+// Configure morgan to use the custom format
+app.use(morgan(customFormat));
 
 let persons = [
   {
@@ -95,7 +114,7 @@ app.get("/info", (req, res) => {
   );
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
