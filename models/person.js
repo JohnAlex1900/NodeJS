23dd40ba036def7bed889af9a7487b65a72ed2b6
John Irungu <johnirungu977@gmail.com>
2024-06-19 11:45:07 +0300

    Error Validation

--- models/person.js
+++ models/person.js
@@ -23,8 +23,13 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minLength: 7,
-    required: true,
+    validate: {
+      validator: function (v) {
+        return /\d{3}-\d{5}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
   },
 });
 
